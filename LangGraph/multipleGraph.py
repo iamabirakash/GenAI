bcd32744b63835c11c6d6d4003c628de9d7c5a21
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2026-07-16 14:21:55 +0530

    feat: add multipleGraph with AgentState and greeting functions

---
+++ LangGraph/multipleGraph.py
@@ -0,0 +1,34 @@
+from typing import TypedDict, List
+from langgraph.graph import StateGraph
+
+class AgentState(TypedDict):
+    name: str
+    age: int
+    msg: List[str]
+
+def greet(state: AgentState)->AgentState:
+    state["msg"].append(f"Hi {state['name']}, How are you?")
+    return state
+
+def greetWithAge(state: AgentState)->AgentState:
+    state["msg"].append(f"Hi {state['name']}, Your age is {state['age']}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("Node1", greet)
+graph.add_node("Node2", greetWithAge)
+graph.add_edge("Node1", "Node2")
+graph.set_entry_point("Node1")
+graph.set_finish_point("Node2")
+
+app = graph.compile()
+result = app.invoke({"name": "Abir", "age": 23, "msg": []})
+print(result["msg"])
+
+
+# student = {
+#     "marks": [100, 90]
+# }
+
+# student["marks"].append("Hello")
+# print(student["marks"])
