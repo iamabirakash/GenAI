a991a3aac9bc74854ef9688f9f3cebf30af695f3
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2026-07-16 10:39:11 +0530

    feat: enhance Student TypedDict with marks field and update print statements

--- LangGraph/staticTyping.py
+++ LangGraph/staticTyping.py
@@ -1,14 +1,17 @@
-from typing import TypedDict
+from typing import TypedDict, List
 
 class Student(TypedDict):
     name: str
     rollno: int
+    marks: List[int]
 
 student : Student = {
     "name": "Abir",
-    "rollno": 23
+    "rollno": 23,
+    "marks": [90, 80, 70]
 }
 
 #PRINT THE VALUES
 print(student["name"])
 print(student["rollno"])
+print(student["marks"])
