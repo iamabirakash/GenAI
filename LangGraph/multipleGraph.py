2fe8cd1981e464cfe0c52363cf2c78e4526d1d62
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2026-07-16 14:25:42 +0530

    feat: update AgentState to use string for msg and modify greet functions

--- LangGraph/multipleGraph.py
+++ LangGraph/multipleGraph.py
@@ -4,14 +4,17 @@ from langgraph.graph import StateGraph
 class AgentState(TypedDict):
     name: str
     age: int
-    msg: List[str]
+    # msg: List[str]
+    msg: str 
 
 def greet(state: AgentState)->AgentState:
-    state["msg"].append(f"Hi {state['name']}, How are you?")
+    # state["msg"].append(f"Hi {state['name']}, How are you?")
+    state["msg"] += f"Hi {state['name']}, How are you?\n"
     return state
 
 def greetWithAge(state: AgentState)->AgentState:
-    state["msg"].append(f"Hi {state['name']}, Your age is {state['age']}")
+    # state["msg"].append(f"Hi {state['name']}, Your age is {state['age']}")
+    state["msg"] += f"Hi {state['name']}, Your age is {state['age']}\n"
     return state
 
 graph = StateGraph(AgentState)
@@ -22,7 +25,8 @@ graph.set_entry_point("Node1")
 graph.set_finish_point("Node2")
 
 app = graph.compile()
-result = app.invoke({"name": "Abir", "age": 23, "msg": []})
+# result = app.invoke({"name": "Abir", "age": 23, "msg": []})
+result = app.invoke({"name": "Abir", "age": 23, "msg": ""})
 print(result["msg"])
 
 
